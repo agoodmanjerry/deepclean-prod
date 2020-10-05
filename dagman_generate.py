436bfcae6e25e7dd12b7810da76d8e5d7b427756
Tri Nguyen <tri.nguyen@ligo.org>
2020-10-05 13:07:50 -0700

    Modify some arguments for dagman_generate.py; organize code

--- dagman_generate.py
+++ dagman_generate.py
@@ -10,24 +10,6 @@ from pycondor import Job, Dagman
 
 from deepclean_prod import io
 
-# Set default parameters 
-TRAIN_PARAMS = ('chanslist', 'train_t0', 'fs', 'train_duration', 'train_frac', 
-                'filt_fl', 'filt_fh', 'filt_order', 'train_kernel', 'train_stride', 
-                'pad_mode', 'batch_size', 'max_epochs', 'num_workers', 'lr', 
-                'weight_decay', 'fftlength', 'overlap', 'psd_weight', 'mse_weight', 
-                'train_dir', 'save_dataset', 'load_dataset', 'device')
-CLEAN_PARAMS = ('chanslist', 'clean_t0', 'clean_duration', 'fs', 'clean_kernel', 
-                'clean_stride', 'pad_mode', 'window', 'train_dir', 'checkpoint', 
-                'ppr_file', 'out_dir', 'out_file', 'out_channel', 'save_dataset', 
-                'load_dataset', 'device')
-
-def get_keys(data, keys):
-    new = {}
-    for k, v in data.items():
-        if k in keys:
-            new[k] = v
-    return new
-
 # Parse command line argument
 def parse_cmd():
     parser = argparse.ArgumentParser(
@@ -42,8 +24,11 @@ params = parse_cmd()
 # Parse config file
 config = io.parse_config(params.config, 'config')
 out_dir = config['out_dir']
-prefix = config['prefix']
-job_name = config['job_name']
+prefix = config.get('prefix', 'prefix')
+job_name = config.get('job_name', 'job')
+max_clean_duration = int(config.get('max_clean_duration', 4096))
+request_memory_training = config.get('request_memory_training', '2 GB')
+request_memory_cleaning = config.get('request_memory_cleaning', '2 GB')
 
 # Create output directory
 os.makedirs(out_dir, exist_ok=True)
@@ -59,10 +44,10 @@ segment_data = np.genfromtxt(config['segment_file'])
 
 # For each segment, create a condor job for training and cleaning
 # default condor
-submit = 'condor/submit'
-log = 'condor/log'
-error = 'condor/error'
-output = 'condor/output'
+submit = os.path.join(out_dir, 'condor/submit')
+log = os.path.join(out_dir, 'condor/log')
+error = os.path.join(out_dir, 'condor/error')
+output = os.path.join(out_dir, 'condor/output')
 getenv = True
 requirements = '(CUDACapability > 3.5)'
 universe = config.get('universe', 'vanilla')
@@ -79,6 +64,7 @@ extra_lines = (
 dagman = Dagman(name='dag_{}'.format(job_name), submit=submit)
 executable_train = os.path.join(os.path.dirname(sys.executable), 'dc-prod-train')
 executable_clean = os.path.join(os.path.dirname(sys.executable), 'dc-prod-clean')
+executable_summary = os.path.join(os.path.dirname(sys.executable), 'dc-prod-summary')
 
 for seg in segment_data:
     
@@ -90,10 +76,9 @@ for seg in segment_data:
     # Get directory for segment        
     segment_subdir = os.path.join(out_dir, '{}-{:d}-{:d}'.format(
         prefix, clean_t0, clean_duration))
-    out_file = '{}-{:d}-{:d}.gwf'.format(prefix, clean_t0, clean_duration)
-        
+
     # Training job
-    train_config = get_keys(config, TRAIN_PARAMS)
+    train_config = io.get_keys(config, io.TRAIN_PARAMS)
     train_config['train_dir'] = segment_subdir
     train_config['train_t0'] = train_t0
     train_config['train_duration'] = train_duration
@@ -111,39 +96,79 @@ for seg in segment_data:
         notification=notification,
         getenv=getenv,
         universe=universe,
-        request_memory = '3 GB',
+        request_memory = request_memory_training,
         extra_lines=extra_lines,
         arguments=[train_args]
     )
 
     # Cleaning job
-    clean_config = get_keys(config, CLEAN_PARAMS)
+    clean_config = io.get_keys(config, io.CLEAN_PARAMS)
     clean_config['train_dir'] = segment_subdir
     clean_config['out_dir'] = segment_subdir
-    clean_config['out_file'] = out_file
-    clean_config['clean_t0'] = clean_t0
-    clean_config['clean_duration'] = clean_duration
-    clean_args = io.dict2args(clean_config)
         
-    job_clean = Job(
-        name='clean_{}'.format(job_name),
-        executable=executable_clean,
-        submit=submit,
-        log=log, 
-        error=error,
-        output=output,
-        dag=dagman,
-        requirements=requirements,
-        notification=notification,
-        getenv=getenv,
-        universe=universe,
-        request_memory = '8 GB',
-        extra_lines=extra_lines,
-        arguments=[clean_args]
-    )
+    t0 = np.arange(clean_t0, clean_t1, max_clean_duration)
+    for i in range(len(t0)):
+        clean_config['clean_t0'] = t0[i]
+        if t0[i] + max_clean_duration > clean_t1:
+            duration = clean_t1 - t0[i]
+        else:
+            duration = max_clean_duration
+        clean_config['clean_duration'] = duration        
+        clean_config['out_file'] = '{}-{:d}-{:d}.gwf'.format(prefix, t0[i], duration)
+
+        clean_args = io.dict2args(clean_config)
+        
+        job_clean = Job(
+            name='clean_{}'.format(job_name),
+            executable=executable_clean,
+            submit=submit,
+            log=log, 
+            error=error,
+            output=output,
+            dag=dagman,
+            requirements=requirements,
+            notification=notification,
+            getenv=getenv,
+            universe=universe,
+            request_memory = request_memory_cleaning,
+            extra_lines=extra_lines,
+            arguments=[clean_args]
+        )
+        job_train.add_child(job_clean)
+
+
+    # Summary page job
+#     summary_config = io.get_keys(config, io.SUMMARY_PARAMS)
+#     summary_config['input_clean'] = os.path.join(segment_subdir, out_file)
+#     summary_config['input_original'] = os.path.join(segment_subdir, 'original.h5')
+#     summary_config['summary_dir'] = os.path.join(segment_subdir, 'html')
+#     summary_config['loss_file'] = os.path.join(segment_subdir, 'metrics/loss.dat')
+#     summary_args = io.dict2args(summary_config)
+
+#     print(summary_args)
     
+#     job_summary = Job(
+#         name='summary_{}'.format(job_name),
+#         executable=executable_summary,
+#         submit=submit,
+#         log=log, 
+#         error=error,
+#         output=output,
+#         dag=dagman,
+#         notification=notification,
+#         getenv=getenv,
+#         universe=universe,
+#         request_memory='8 GB',
+#         extra_lines=(
+#             'accounting_group = {}'.format(accounting_group),
+#             'stream_output = True',
+#             'stream_error = True'),
+#         arguments=[summary_args]
+#     )
+        
     # Inter-job training -> cleaning
-    job_train.add_child(job_clean)
+#     job_train.add_child(job_clean)
+#     job_clean.add_child(job_summary)
 
 if params.submit:
     dagman.build_submit()
